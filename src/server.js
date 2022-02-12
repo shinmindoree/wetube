1c833c095f4e7513adbb2e56581f6eaa55fcf03b
mindoree <elgd00@gmail>
2022-02-12 22:47:56 +0900

    initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
+
+// function handleListening(){
+//     console.log(`Server listening on http://localhost:${PORT}`)
+// }
+
+app.listen(PORT, handleListening)
+
+console.log("HI ALL!!!"); 
